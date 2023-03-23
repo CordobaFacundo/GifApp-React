f2cf6cb89376d0f48629ba50e46a8dfb81602167
unknown <facundo.cordoba2012@gmail.com>
2023-03-23 20:00:09 -0300

    getGifs

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,24 +1,27 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import { AddCatergory } from './components/AddCatergory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifApp = () => {
 
-    const [categories, setCategories] = useState(['Dragon ball', 'Death note']);
+    const [categories, setCategories] = useState(['Dragon ball']);
     const onAddCategory = (newCategory) => {
-        setCategories([newCategory, ...categories])
+        if (categories.includes(newCategory)) return;
+        setCategories([newCategory, ...categories]);
     }
 
     return (
         <>
             <h1>Gif App</h1>
 
-            <AddCatergory onNewCategory = {onAddCategory} />
-            
-            <ol>
-                {categories.map( (category) => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
+            <AddCatergory onNewCategory={onAddCategory} />
+
+            {
+                categories.map((category) => (
+                    <GifGrid key={category} category={category} />
+                ))
+            }
+
         </>
     )
 }
